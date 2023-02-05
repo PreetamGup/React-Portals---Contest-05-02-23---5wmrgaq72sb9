a9b69a1d5103a0da641a91a4ca73a891516c407d
Preetam Gupta <106731801+PreetamGup@users.noreply.github.com>
2023-02-05 19:01:31 +0530

    latest

--- src/components/App.js
+++ src/components/App.js
@@ -1,22 +1,32 @@
-import React ,{useState, useRef} from 'react'
-import '../styles/App.css';
-import PortalButton from './PortalButton.js';
-import PortalTextArea from './PortalTextArea.js';
+import React, { useState, useRef } from "react";
+// import '../styles/App.css';
+import PortalButton from "./PortalButton.js";
+import PortalTextArea from "./PortalTextArea.js";
 
-//complete the function 
+//complete the function
 const App = () => {
+  const [state, setstate] = useState("");
+   const [text, settext] = useState("");
 
-
+  // let text='';
+  const handleInput = () => {
+   settext(state);
+    setstate("");
+  };
 
   return (
-    
     <div id="main">
-      <input id="input" value={} onChange={}></input><br/>
-      <PortalButton buttonclick={} /><br/>
-      <PortalTextArea value={}/>
+      <input
+        id="input"
+        value={state}
+        onChange={(e) => setstate(e.target.value)}
+      ></input>
+      <br />
+      <PortalButton buttonclick={handleInput} />
+      <br />
+      <PortalTextArea value={text} />
     </div>
-  )
-}
-
+  );
+};
 
 export default App;
--- src/components/PortalTextArea.js
+++ src/components/PortalTextArea.js
@@ -1,13 +1,13 @@
-import React from 'react';
-import  ReactDOM  from 'react-dom';
+import React from "react";
+import ReactDOM from "react-dom";
 
 //complete this function using portals
-const PortalTextArea=()=>{
-    return (
-        <div>
-            <textarea id="textarea" value={}></textarea>
-        </div>
-    )
-}
+const PortalTextArea = (props) => {
+  return (
+    <div>
+      <textarea id="textarea" value={props.value}></textarea>
+    </div>
+  );
+};
 export default PortalTextArea;
 //portal-textarea id will be used here for portal purpose
